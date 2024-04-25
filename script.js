ee5bb663f83358246e0324c74d6698a601de8a1d
0x_sinsu <157507447+0x-sinsu@users.noreply.github.com>
2024-04-25 20:32:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const canvas = createCanvas(1080, 720);
 const context = canvas.getContext('2d');
 
 // ローカルフォントのパスとファミリー名
-const fontPath = ./MPLUSRounded1c-Regular.ttf;
+const fontPath = "./MPLUSRounded1c-Regular.ttf";
 const fontFamily = "Rounded Mplus 1c";
 
 // ローカルフォントを読み込む
