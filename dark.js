2a713a4baf94def21c06d8b27364fe6b9a1c40eb
cc5efd7b0 <157507447+0x-sinsu@users.noreply.github.com>
2024-05-11 20:52:24 +0900

    Update dark.js

--- dark.js
+++ dark.js
@@ -1,8 +1,8 @@
 const dayjs = require('dayjs')
-const utc = require('dayjs/plugin/jst')
+const utc = require('dayjs/plugin/utc')
 const timezone = require('dayjs/plugin/timezone')
 
-dayjs.extend(jst)
+dayjs.extend(utc)
 dayjs.extend(timezone)
 
 dayjs.tz.setDefault("Asia/Tokyo")
